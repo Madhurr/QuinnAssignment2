b32053aea8048e0fbef3807c9befdd92b0b263f5
Madhur <madhur@Madhurs-MacBook-Pro.local>
2019-10-23 02:53:59 +0530

    URLRequest JSON Parsing inside Model

--- newsReaderAssignment2/ViewController.swift
+++ newsReaderAssignment2/ViewController.swift
@@ -11,16 +11,72 @@ import UIKit
 class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
    
     
-
+    
+ 
     @IBOutlet weak var tableView: UITableView!
+    
+    
+    var articles: [Article]? = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fetchArticals()
     }
+    
+    // This function is responsible for fetching News Data as JSON and map it to Article.swift as model class
+    func fetchArticals(){
+        let urlRequest = URLRequest(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0d7750a250ae493392c5f7c973c9f19d")!)
+        
+        // When task is executing it will call a closure it will capture data and response or if request fails it will also throw error
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data,response,error) in
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            // Creating array of object of article
+            self.articles = [Article]()
+            // data Serilization of JSON
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                
+                // casting json artical's array into array of dictonaries
+                
+                if let articelsFromJSon = json["articles"] as? [[String : AnyObject]] {
+                    for articleFromJSon in articelsFromJSon{
+                        let article = Article()
+                        if let title = articleFromJSon["title"] as? String, let author = articleFromJSon["author"] as? String, let desc = articleFromJSon["description"] as? String, let url = articleFromJSon["url"] as? String , let urlToImage = articleFromJSon["urlToImage"] as? String {
+                            article.author = author
+                            article.headLine = title
+                            article.desc  = desc
+                            article.url = url
+                            article.imageUrl = urlToImage
+                        }
+                        self.articles?.append(article)
+                    }
+                }
+                DispatchQueue.main.async {
+                  self.tableView.reloadData()
+                }
+            }catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell  = tableView.dequeueReusableCell(withIdentifier: "articleCell" , for: indexPath) as! ArticleCell
-        cell.title.text = "This is the Test"
+        cell.title.text = self.articles?[indexPath.item].headLine
+        cell.desc.text = self.articles?[indexPath.item].desc
+        cell.author.text = self.articles?[indexPath.item].author
+        
         return cell
        }
     
@@ -29,7 +85,7 @@ class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSo
        }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-              return 1
+        return self.articles?.count ?? 0
           }
 
 }
