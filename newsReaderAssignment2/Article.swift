c89f7bd9e9a653297872cd58b05258c1debdfde4
Madhur <madhur@Madhurs-MacBook-Pro.local>
2019-10-22 22:06:23 +0530

    TableView and outlets setup

---
+++ newsReaderAssignment2/Article.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  newsReaderAssignment2
+//
+//  Created by Madhur on 22/10/19.
+//  Copyright © 2019 Madhur. All rights reserved.
+//
+
+import UIKit
+
+class Article: NSObject {
+ 
+    var headLine: String?
+    var desc: String?
+    var author: String?
+    var url: String?
+    var imageUrl: String?
+    
+    
+}
--- newsReaderAssignment2/ArticleCell.swift
+++ newsReaderAssignment2/ArticleCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ArticleCell: UITableViewCell {
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var author: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
