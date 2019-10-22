cd62e36fd53c6e90e5d9390498b18483d304ddcb
Madhur <madhur@Madhurs-MacBook-Pro.local>
2019-10-22 21:35:26 +0530

    basic UI Setup

---
+++ newsReaderAssignment2/ArticleCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArticleCell.swift
+//  newsReaderAssignment2
+//
+//  Created by Madhur on 22/10/19.
+//  Copyright © 2019 Madhur. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ newsReaderAssignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsReaderAssignment2
+//
+//  Created by Madhur on 22/10/19.
+//  Copyright © 2019 Madhur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
